5a04ec94373aca9a6ee6a077a2e61d5b534bbfb9
Anderson Nieves <andersonvnieves@live.com>
2021-03-15 20:47:38 -0300

    Estrutura Inicial

---
+++ CampanhaCovid.Backend.Domain/Services/RegistraUsuarioService.cs
@@ -0,0 +1,25 @@
+﻿using CampanhaCovid.Backend.Domain.Entities;
+using CampanhaCovid.Backend.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampanhaCovid.Backend.Domain.Services
+{
+    public class RegistraUsuarioService
+    {
+        private readonly IUsuarioRepository<Usuario> usuarioRepository;
+
+        public RegistraUsuarioService(IUsuarioRepository<Usuario> usuarioRepository)
+        {
+            this.usuarioRepository = usuarioRepository;
+        }
+
+        public void Executar()
+        {
+
+        }
+    }
+}
---
+++ CampanhaCovid.Backend.Infrastructure/Persistence/Repositories/UsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using CampanhaCovid.Backend.Domain.Entities;
+using CampanhaCovid.Backend.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampanhaCovid.Backend.Infrastructure.Persistence.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository<Usuario>
+    {
+        public void Salvar(Task entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
